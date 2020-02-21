935215ad232559592995fa8ed36df5958b80022a
Wadson Vaval <34847736+Wadprog@users.noreply.github.com>
2020-02-20 20:30:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express(); 
 app.get('/', (req, res)=> res.send('We are working good'))
+app.use('/api/usuarios', require('./routes/api/usuarios'))
+app.use('/api/prestamos', require('./routes/api/prestamos'))
+app.use('/api/clientes', require('./routes/api/clientes'))
+
 const PORT = process.env.Port || 6000; 
 app.listen(PORT, ()=> console.log(`Server runing on port ${PORT}`)); 
