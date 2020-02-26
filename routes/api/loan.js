521d850c5c68b859432ada636ed2d8c79e17ebec
Wadson Vaval <vavalson@hotmail.com>
2020-02-26 11:29:40 -0400

    added routine routes to loan

--- routes/api/loan.js
+++ routes/api/loan.js
@@ -11,8 +11,9 @@ const Loan = require('../../models/loan');
 //@desc access public temp
 router.get('/', async (req, res) => {
 	try {
-		let loans = await Loan.find().populate(['plan','client']);
-		res.json(loans);
+		let loans = await Loan.find().populate([ 'plan', 'client' ]);
+
+		res.json({ count: loans.length, loans });
 	} catch (error) {
 		console.log(`Get not complete task get all Loan`);
 		res.json({ msg: 'Server error ${error}' });
@@ -24,29 +25,64 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
 	try {
-		let loan = await Loan.findById(req.params.id).populate(['plan','client']);
+		let loan = await await Loan.findById(req.params.id).populate([ 'plan', 'client' ]);
 		if (!loan) res.status(404).json({ msg: 'This loan does not exist' });
 		res.json(loan);
 	} catch (error) {
-		console.log(`Could not get this loan ${req.params.id}`);
-		res.json({ msg: 'Server error ${error}' });
+		console.log(`Could not get this loan ${req.params.id} and error ${error}`);
+		res.json({ msg: `Server error ${error}` });
 	}
 });
 //@routes get api/Loan/:id
 //@desc Get  a  Loan by id route
 //@desc access public temp
 
-
-router.get('/client/:id', async (req, res)=>{
+router.get('/client/:id', async (req, res) => {
 	try {
-		let loans = await Loan.find({client:req.params.id}).populate('plan');
-		res.json(loans)
-		
+		let loans = await Loan.find({ client: req.params.id }).populate('plan');
+		res.json(loans);
 	} catch (error) {
 		console.log(`Get not complete task get all Loan`);
 		res.json({ msg: `Server error ${error}` });
 	}
-})
+});
+
+//@routes get api/Loan/routine
+//@desc Get  all non debt collectable
+//@desc access public temp
+
+router.get('/get/routine/', async (req, res) => {
+	try {
+		const limitdate = 1;
+		// need to use moment to create acurate date calc
+		let today = Date.now() - limitdate;
+		// need to fix querry 
+		let loans = await Loan.find({
+			$or: [
+				{ estadoPago: false, pagos: [], fecha: { $lt: today } },
+				{ estadoPago: false, pagos: { $exists: true, $ne: [] }, monto: { $gt: 1000 } }
+			]
+		}).populate([ 'client', 'plan' ]);
+
+		const cities = loans.reduce(
+			(unique, loan) => (unique.includes(loan.client.ciudad) ? unique : [ ...unique, loan.client.ciudad ]),
+			[]
+		);
+		let response = [];
+		cities.forEach(city => {
+			let tempCity = loans.reduce(
+				(allLoans, loan) => (loan.client.ciudad != city ? allLoans : [ ...allLoans, loan ]),
+				[]
+			);
+			response.push({ [city]: tempCity });
+		});
+
+		res.json({ count: loans.length, response });
+	} catch (error) {
+		console.log(`Get not complete task get all Loan`);
+		res.json({ msg: `Server error ${error}` });
+	}
+});
 //@routes post api/Loan/
 //@desc Create new  Loan route
 //@desc access public temp
--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(express.json({extended:false}));
 // connection to database; 
 connectDB(); 
 
+// Access to all origin with CORS 
+app.use((req,res,next)=>{
+ res.header('Access-Control-Allow-Origin','*')
+ res.header('Access-Control-Allow-Headers',"Origin, X-Request-With,Content-Type, Accept, Authorization")
+ if(req.method==='OPTIONS'){
+ res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET')
+ return res.status(200).json({})
+ }
+next();
+})
 //use the routes
 
 app.use('/api/client',require('./routes/api/client'))
