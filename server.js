a06f2e2353995252a168521b13636c5b82c37875
Wadprog <Vavalson@hotmail.com>
2020-02-27 17:04:18 -0400

    deploy init

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const express = require('express');
+const express = require('express')
+// in deploy
+const connectDB = require('./config/db')
 
-const connectDB= require('./config/db')
+const app = express()
+app.use(express.json({ extended: false }))
 
-const app = express(); 
-app.use(express.json({extended:false}));
-
-// connection to database; 
-connectDB(); 
+// connection to database;
+connectDB()
 
 //use the routes
 
-app.use('/api/client',require('./routes/api/client'))
-app.use('/api/plan',require('./routes/api/plan'))
-app.use('/api/loan',require('./routes/api/loan'))
-app.use('/api/user',require('./routes/api/user'))
+app.use('/api/client', require('./routes/api/client'))
+app.use('/api/plan', require('./routes/api/plan'))
+app.use('/api/loan', require('./routes/api/loan'))
+app.use('/api/user', require('./routes/api/user'))
 
-
-app.get('/', (req, res)=> res.send('We are working good'))
-const PORT = process.env.Port || 6000; 
-app.listen(PORT, ()=> console.log(`Server runing on port ${PORT}`)); 
+app.get('/', (req, res) => res.send('We are working good'))
+const PORT = process.env.Port || 6000
+app.listen(PORT, () => console.log(`Server runing on port ${PORT}`))
