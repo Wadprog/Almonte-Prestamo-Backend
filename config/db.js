aca106c2396f6d68855c2f089fa4819e8383fe71
Wadson Vaval <vavalson@hotmail.com>
2020-03-02 23:16:30 -0400

    first try commit

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
     try {
        await mongoose.connect(db,{
            useNewUrlParser:true,
-           useUnifiedTopology: true 
+           useUnifiedTopology: true ,
+           useCreateIndexes:true
        })
        console.log('MongoDb connected successfully'); 
     } catch (error) {
