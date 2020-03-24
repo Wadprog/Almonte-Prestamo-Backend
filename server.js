9208fbc8ee85d400ca1a00c3916e0950eb67d323
Wadson Vaval <vavalson@hotmail.com>
2020-03-24 17:15:22 -0400

    '

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('index', { test: 'sister' }));
 
-const PORT = process.env.Port || 5000;
+const PORT = process.env.Port || 80;
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
