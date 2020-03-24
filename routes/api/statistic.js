b9c04d9dd4c5ed418a875b7db7730771a1c9e79a
Wadson Vaval <vavalson@hotmail.com>
2020-03-24 16:37:10 -0400

    added expenses to statistic

--- routes/api/statistic.js
+++ routes/api/statistic.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Payment = require('../../models/payment');
 const Loan = require('../../models/loan');
+const Expense = require('../../models/expense');
 
 /*----------------------------------------------------------
                          Routes
@@ -52,7 +53,9 @@ const totalPerMonth = (items, months) => {
 			[]
 		);
 		itemInMonths.push({
-			name: monthsname[month], total: tempmonth.reduce((total, loan) => total + loan.amount, 0)
+			position: month,
+			name: monthsname[month],
+			total: tempmonth.reduce((total, loan) => total + loan.amount, 0)
 		});
 	});
 	return itemInMonths;
@@ -77,14 +80,14 @@ const PaymenttotalPerMonth = (items, months) => {
 
 	months.forEach(month => {
 		let tempmonth = items.reduce(
-			(allItems, item) => (new Date(item.dateInterestPaid).getMonth() != month ? allItems : [ ...allItems, item ]),
+			(allItems, item) =>
+				new Date(item.dateInterestPaid).getMonth() != month ? allItems : [ ...allItems, item ],
 			[]
 		);
 		itemInMonths.push({
-			name: monthsname[month], total: tempmonth.reduce(
-				(total, element) => total + element.amountPaid + element.interestPaid,
-				0
-			)
+			position: month,
+			name: monthsname[month],
+			total: tempmonth.reduce((total, element) => total + element.amountPaid + element.interestPaid, 0)
 		});
 	});
 	return itemInMonths;
@@ -96,13 +99,18 @@ const PaymenttotalPerMonth = (items, months) => {
 router.get('/', async (req, res) => {
 	try {
 		let loans = await Loan.find();
-		loans= loans.filter(loan=> new Date(loan.date).getFullYear()== new Date().getFullYear()); 
+		loans = loans.filter(loan => new Date(loan.date).getFullYear() == new Date().getFullYear());
 		const loanMonthsTotal = totalPerMonth(loans, montInObject(loans));
+
+		let expenses = await Expense.find();
+		expenses = expenses.filter(expense => new Date(expense.date).getFullYear() == new Date().getFullYear());
+		const expenseMonthsTotal = totalPerMonth(expenses, montInObject(expenses));
+
 		let payments = await Payment.find();
 		payments = payments.filter(payment => payment.status != 'unpaid');
 		const paymentMonthsTotal = PaymenttotalPerMonth(payments, montInPayment(payments));
 
-		res.json({ loanMonthsTotal, paymentMonthsTotal });
+		res.json({ loanMonthsTotal, paymentMonthsTotal, expenseMonthsTotal });
 	} catch (error) {
 		console.log(`Get not complete task get statistic`);
 		res.status(400).json({ msg: `Server error ${error}` });
