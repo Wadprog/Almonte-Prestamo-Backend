5e1ce34be0cc75dbfe29014ee34caf0451c6b53d
Wadson Vaval <vavalson@hotmail.com>
2020-03-24 21:47:11 -0400

    build for final testing

---
+++ client/build/precache-manifest.3ccc8d4e9bc4d824059669f9bdc42a98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ac8a2fff045477f927423502d6a0ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a65fe386a9ade5ea7ac",
+    "url": "/static/css/main.e04f9629.chunk.css"
+  },
+  {
+    "revision": "72345ae09d9030121b63",
+    "url": "/static/js/2.700a32f9.chunk.js"
+  },
+  {
+    "revision": "73ab36127f504ad78636e9c40d2d1aab",
+    "url": "/static/js/2.700a32f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a65fe386a9ade5ea7ac",
+    "url": "/static/js/main.e785bc62.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// in deploy
+const path = require('path');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -34,9 +34,13 @@ app.use('/api/statistic', require('./routes/api/statistic'));
 app.use('/api/expense', require('./routes/api/expense'));
 app.use('/api/clientwloan', require('./routes/api/clientwloan'));
 
-app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('index', { test: 'sister' }));
 
-const PORT = process.env.Port || 80;
+app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		});
+		
+const PORT = process.env.Port || 5000;
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
