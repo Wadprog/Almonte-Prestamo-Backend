7e6ac8b6145e4b17aef1675166b691daec2f6740
Wadson Vaval <vavalson@hotmail.com>
2020-04-14 01:28:22 -0400

    built created

---
+++ client/build/precache-manifest.00bc1b423938113eb12f4a854358e293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7abf499c3dc21213aef61388c1d8a9e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "827615d51b152d07da6d",
+    "url": "/static/css/main.1bd38e6f.chunk.css"
+  },
+  {
+    "revision": "06242483a1ca212a5e06",
+    "url": "/static/js/2.ebad0bcd.chunk.js"
+  },
+  {
+    "revision": "73ab36127f504ad78636e9c40d2d1aab",
+    "url": "/static/js/2.ebad0bcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "827615d51b152d07da6d",
+    "url": "/static/js/main.7e6e9ef6.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
